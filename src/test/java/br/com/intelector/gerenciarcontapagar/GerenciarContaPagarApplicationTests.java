05370ef6968e7246e22337121a32f1111290f879
t.alessandro <thiago.alessandro@totvs.com.br>
2020-09-29 22:15:07 -0300

    Teste pra validar CI

--- src/test/java/br/com/intelector/gerenciarcontapagar/GerenciarContaPagarApplicationTests.java
+++ src/test/java/br/com/intelector/gerenciarcontapagar/GerenciarContaPagarApplicationTests.java
@@ -10,7 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.fail;
 
-@SpringBootTest
+//@SpringBootTest
 class GerenciarContaPagarApplicationTests {
 
     @Autowired
